29186ea41a0a9804f39eab6f3618910efbabfb3c
Minesweper <ybh20030214@qq.com>
2024-07-23 12:40:46 +0800

    drop

--- src/observer/sql/stmt/drop_index_stmt.h
+++ src/observer/sql/stmt/drop_index_stmt.h
@@ -5,5 +5,10 @@
 
 class DropIndexStmt : public Stmt
 {
+public:
   StmtType type() const override { return StmtType::DROP_INDEX; }
+  virtual ~DropIndexStmt() = default;
+  DropIndexStmt(const std::string &table_name) : table_name_(table_name){};
+  static RC          create(Db *db, const DropTableSqlNode &drop_table, Stmt *&stmt);
+  const std::string &table_name() const { return table_name_; }
 };
