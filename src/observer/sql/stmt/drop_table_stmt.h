46a7210e7ea3c7b8e3353a604b53eca1682a4e6a
Minesweper <ybh20030214@qq.com>
2024-07-23 12:19:51 +0800

    drop

--- src/observer/sql/stmt/drop_table_stmt.h
+++ src/observer/sql/stmt/drop_table_stmt.h
@@ -9,7 +9,6 @@ public:
   virtual ~DropTableStmt() = default;
   DropTableStmt(const std::string &table_name):table_name_(table_name){};
   static RC create(Db *db, const DropTableSqlNode &drop_table, Stmt *&stmt);
-
   const std::string &table_name() const { return table_name_; }
 
 
