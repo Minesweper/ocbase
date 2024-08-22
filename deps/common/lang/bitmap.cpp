e41dabc5ca9d4d499e395fe7886529a989a82e8a
Minesweper <ybh20030214@qq.com>
2024-08-22 21:07:44 +0800

    expr

--- deps/common/lang/bitmap.cpp
+++ deps/common/lang/bitmap.cpp
@@ -47,7 +47,7 @@ void Bitmap::init(char *bitmap, int size)
   size_   = size;
 }
 
-bool Bitmap::get_bit(int index)
+bool Bitmap::get_bit const(int index)
 {
   char bits = bitmap_[index / 8];
   return (bits & (1 << (index % 8))) != 0;
