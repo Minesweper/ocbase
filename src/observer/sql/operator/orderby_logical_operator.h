b4228dfc69687f98770c33cbcb7e8842d39231bc
Minesweper <ybh20030214@qq.com>
2024-08-27 13:10:43 +0800

    update

--- src/observer/sql/operator/orderby_logical_operator.h
+++ src/observer/sql/operator/orderby_logical_operator.h
@@ -14,7 +14,7 @@
 class OrderByLogicalOperator : public LogicalOperator
 {
 public:
-  OrderByLogicalOperator::OrderByLogicalOperator(
+  OrderByLogicalOperator(
       std::vector<std::unique_ptr<OrderByUnit>> &&orderby_units, std::vector<std::unique_ptr<Expression>> &&exprs)
       : orderby_units_(std::move(orderby_units)), exprs_(std::move(exprs))
   {}
