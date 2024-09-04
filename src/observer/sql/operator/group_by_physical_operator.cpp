2fe0e3f818726583529548194c12be3daec90c0c
Minesweper <ybh20030214@qq.com>
2024-09-04 12:52:46 +0800

    aggr

--- src/observer/sql/operator/group_by_physical_operator.cpp
+++ src/observer/sql/operator/group_by_physical_operator.cpp
@@ -33,7 +33,7 @@ void GroupByPhysicalOperator::create_aggregator_list(AggregatorList &aggregator_
   aggregator_list.clear();
   aggregator_list.reserve(aggregate_expressions_.size());
   ranges::for_each(aggregate_expressions_, [&aggregator_list](std::unique_ptr<Expression> expr) {
-    auto aggregate_expr = dynamic_pointer_cast<AggregateExpr>(expr);
+    auto aggregate_expr = static_cast<AggregateExpr *>(expr.get());
     aggregator_list.emplace_back(aggregate_expr->create_aggregator());
   });
 }
