a325d6bb8cfdd174a4d3bd4a41f53f6a045dfb76
Minesweper <ybh20030214@qq.com>
2024-09-04 12:28:08 +0800

    aggr

--- src/observer/sql/operator/group_by_physical_operator.cpp
+++ src/observer/sql/operator/group_by_physical_operator.cpp
@@ -34,7 +34,7 @@ GroupByPhysicalOperator::GroupByPhysicalOperator(vector<Expression *> &&expressi
 }
 GroupByPhysicalOperator::GroupByPhysicalOperator(std::vector<std::unique_ptr<Expression>> &&groupby_fields,
     std::vector<std::unique_ptr<AggrFuncExpr>> &&agg_exprs, std::vector<std::unique_ptr<FieldExpr>> &&field_exprs)
-    : aggregate_expressions_(std::move(groupby_fields))
+    : aggregate_expressions_(groupby_fields)
 {
   tuple_.init(std::move(agg_exprs), std::move(field_exprs));
 }
